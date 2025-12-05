c2463fe60d5541217d08f1c14a7dd16ecf11768b
Daniel V. de Macedo <danielvalentemacedo@gmail.com>
2025-12-05 14:54:29 +0100

    day 05: part 1

--- 2025/05/day_05.py
+++ 2025/05/day_05.py
@@ -1,17 +1,35 @@
+fresh_range = []
 input_data = []
 
 def read_input_data():
     f = open("input/data.txt", "r")
+    reading_range = True
     for i in f:
-        input_data.append(i.strip().replace('\r', '').replace('\n', ''))
+        row = i.strip().replace('\r', '').replace('\n', '')
+        if row == '':
+            reading_range = False
+            continue
+
+        if reading_range:
+            row_split = i.split('-')
+            range_tuple = (int(row_split[0]), int(row_split[1]))
+            fresh_range.append(range_tuple)
+        else:
+            input_data.append(int(row))
 
 def do_part_1():
-    print(input_data)
+    set_of_fresh_ids = set()
+    for range in fresh_range:
+        for value in input_data:
+            if range[0] <= value <= range[1]:
+                set_of_fresh_ids.add(value)
+
+    return len(set_of_fresh_ids)
 
 def do_part_2():
     pass
 
 if __name__ == '__main__':
     read_input_data()
-    do_part_1()
+    print(do_part_1())
     #do_part_2()
