4e0d7fe28600756a06b5c90ba738c69d786f3779
Daniel Macedo <danielvalentemacedo@gmail.com>
2025-12-07 00:02:00 +0100

    day 06: part 2

--- 2025/06/day_06.py
+++ 2025/06/day_06.py
@@ -4,10 +4,14 @@ input_data = []
 
 def read_input_data():
     f = open("input/data.txt", "r")
+    temp_input_data = []
     for i in f:
-        i = re.sub(r'\s+', ' ', i)
-        i = i.strip().replace('\r', '').replace('\n', '')
-        input_data.append(i.split(' '))
+        i = i.replace('\r', '').replace('\n', '')
+        temp_input_data.append(i)
+
+    maxlen = max(len(l) for l in temp_input_data)
+    for l in temp_input_data:
+        input_data.append(l.ljust(maxlen, ' '))
 
 #4951502530386
 def do_part_1():
@@ -15,7 +19,6 @@ def do_part_1():
     rows = len(input_data)
 
     total = 0
-
     for col in range(0, columns):
         operation = input_data[rows - 1][col]
         total_operation = int(input_data[0][col])
@@ -29,10 +32,59 @@ def do_part_1():
 
     return total
 
+def get_operations():
+    starts_and_sizes = []
+    rows = len(input_data)
+    operations = input_data[rows - 1]
+    current_operation = operations[0]
+    current_operation_s = 0
+
+    for idx, i in enumerate(operations[1:]):
+        idx = idx + 1
+        if i == '*' or i == '+':
+            starts_and_sizes.append((current_operation, current_operation_s, idx - current_operation_s - 1))
+            current_operation_s = idx
+            current_operation = i
+    starts_and_sizes.append((current_operation, current_operation_s, len(operations) - current_operation_s))
+    return starts_and_sizes
+
+def get_numbers_from_operation(operation):
+    horizontal_numbers = []
+    columns = operation[2]
+    for row in input_data[:-1]:
+        horizontal_numbers.append(row[operation[1]:operation[1]+columns])
+
+    vertical_numbers = []
+    rows = len(horizontal_numbers)
+    for col in range(0, columns):
+        number = ''
+        for row in horizontal_numbers:
+            number += row[col]
+        vertical_numbers.append(int(number))
+
+    return vertical_numbers
+
+#8486156119946
 def do_part_2():
-    pass
+    operations = get_operations()
+
+    total = 0
+    for operation in operations:
+        #print(operation)
+        numbers = get_numbers_from_operation(operation)
+        #print(numbers)
+        total_operation = numbers[0]
+        for number in numbers[1:]:
+            if operation[0] == '*':
+                total_operation *= number
+            elif operation[0] == '+':
+                total_operation += number
+        total += total_operation
+
+    print(total)
+
 
 if __name__ == '__main__':
     read_input_data()
-    print(do_part_1())
-    #do_part_2()
+    #print(do_part_1())
+    do_part_2()
